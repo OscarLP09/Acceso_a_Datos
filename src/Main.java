bfe7933d5ab71da82aa47c5bac39b3c242063177
OscarLuquePorca <oscarluqueporca@gmail.com>
2024-09-17 10:30:33 +0200

    fffffff

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,60 @@
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
 
-        var listado = new ArrayList<Integer>();
+        /* Mirar esto
+        ArrayList<Data> listado = new ArrayList();
 
-        listado.add(1);
-        listado.add(2);
-        listado.add(3);
-        listado.add(4);
+        listado.add(new Data());
+        listado.add(new Data());
+        listado.add(new Data());
+        listado.add(new Data());
 
         System.out.println(listado);
 
         listado.addAll(listado);
 
-        System.out.println(listado);
+        System.out.println(listado.get(0));
+
+        listado.add(new Data());
+
+        listado.get(1).name = "Francisco Romero";
+
+        System.out.println(listado.remove());
+        */
+
+        var conjunto = new HashSet<Integer>();
+
+        conjunto.add(1);
+        conjunto.add(2);
+        conjunto.add(3);
+        conjunto.add(4);
+        conjunto.add(10);
+        conjunto.add(22);
+
+        System.out.println(conjunto);
+
+        conjunto.forEach(
+                (i)->{
+                    if(i > 9) {
+                        System.out.println(i);
+                    }
+                }
+        );
+
+        /*for(Integer i : conjunto){
+            System.out.println(1);
+
+            var it = conjunto.iterator();// ayuda a iterar
+            while (it.hasNext()) {
+                System.out.println(it.next());
+            }
+        }  */
     }
 }
