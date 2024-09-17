819b72b106b94d59fe557b09929ace3053221264
OscarLuquePorca <oscarluqueporca@gmail.com>
2024-09-17 09:54:02 +0200

    fffffff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        var listado = new ArrayList<Integer>();
+
+        listado.add(1);
+        listado.add(2);
+        listado.add(3);
+        listado.add(4);
+
+        System.out.println(listado);
     }
 }
