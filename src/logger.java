383a7fb23bbeaaef25b00b306b57004dfcdc6b35
OscarLuquePorca <oscarluqueporca@gmail.com>
2024-09-18 10:18:42 +0200

    New Data fixed

--- src/logger.java
+++ src/logger.java
@@ -1,9 +1,21 @@
-import javax.xml.crypto.Data;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 // Mirar que es el New Data
 public class logger {
+    public static class Data {
+        private String info;
+
+        public Data() {
+            this.info = info;
+        }
+
+        @Override
+        public String toString() {
+            return "Data: " + info;
+        }
+    }
+
 
     // Se define un objeto estático "log" de tipo Logger, que se utiliza para registrar mensajes.
     // Se asocia al nombre de la clase Main (parece que debería ser la clase Logger, pero el nombre Main está mal).
@@ -22,7 +34,9 @@ public class logger {
 
         // Registra un mensaje en el nivel INFO con un objeto Data, aunque no hace nada útil con el Data.
         // "new Data()" crea un nuevo objeto Data, pero no es claro su propósito aquí.
-        log.log(Level.INFO, "Nuevo dato", new Data() {});
+        log.log(Level.INFO, "Nuevo dato", new Data());
+
+
 
     }
 }
