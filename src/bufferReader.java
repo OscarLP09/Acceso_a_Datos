b749e67f585f9840ecfff8b0d14025d551f6eb36
OscarLuquePorca <oscarluqueporca@gmail.com>
2024-09-19 11:57:18 +0200

    File y bufferReader

--- src/bufferReader.java
+++ src/bufferReader.java
@@ -1,18 +1,35 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+
 //
 public class bufferReader {
-    public static void main(String[] args){
-        try(BufferedReader bfr = new BufferedReader(
-                new FileReader("input.txt"))
-        ){
 
-        } catch (FileNotFoundException e){
-            throw new RuntimeException(e);
-        } catch (IOException e){
-            throw new RuntimeException(e);
+    public static void main(String[] args) {
+        String archivo;
+        String archivo2;
+        private static void copiarArchivoTexto(String archivo, String archivo2){
+            try(BufferedReader bfr = new BufferedReader(new FileReader(archivo));
+            BufferedWriter bfw = new BufferedWriter(new FileWriter(archivo2));
+            )}
+
+        private static void leerArchivo2 (String archivo){
+            try (BufferedReader bfr = new BufferedReader(
+                    new FileReader(archivo))
+            ) {
+                Integer lineas = bfr.lines().toList();
+                System.out.println(lineas);
+            /*String s;
+            Integer contador=0;
+            while ((s=bfr.readLine())!=null){
+                System.out.println(s.toUpperCase());
+                contador++;
+            };
+            System.out.println("El archivo tiene " + contador + " lineas");*/
+
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
